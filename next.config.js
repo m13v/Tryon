6e46abd44a0800c407a5ae0ad13c8112e93b7e24
Matthew Diakonov <104702220+matthew-heartful@users.noreply.github.com>
2023-12-29 07:12:11 +0300

    off eslint

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@ const {webpack} = require("next/dist/compiled/webpack/webpack");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   webpack: (config, {isServer}) => {
     if (!isServer) {
         config.resolve = {
