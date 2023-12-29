4be703fafccef60cc1fd7aff8a25c8aa2329e916
Matthew Diakonov <104702220+matthew-heartful@users.noreply.github.com>
2023-12-29 09:57:43 +0300

    init GA

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,23 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
+        {/* <!-- Google tag (gtag.js) --> */}
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-9K94NYM2MP" />
+        <Script id="google-analytics">
+          {`window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-9K94NYM2MP');`}
+        </Script>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
