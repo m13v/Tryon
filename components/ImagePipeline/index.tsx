082132185a10187eb86a4e8c71aa92b3d98da90e
江纬 <wei@algmon.com>
2024-02-15 20:29:12 +0800

    Update api endpoint for virtual tryon service

--- components/ImagePipeline/index.tsx
+++ components/ImagePipeline/index.tsx
@@ -10,105 +10,105 @@ const apiUrls = ["/load_example", "/load_example_1", "/load_example_2"];
 const top_garment = [
   {
     id: 0,
-    url: "https://humanaigc-outfitanyone.hf.space/--replicas/o90fr/file=/tmp/gradio/f3ba321017355c5a0c8051d3f438e881cc87b945/top222.JPG",
+    url: "https://humanaigc-outfitanyone.hf.space/--replicas/ppht9/file=/tmp/gradio/f3ba321017355c5a0c8051d3f438e881cc87b945/top222.JPG",
   },
   {
     id: 1,
-    url: "https://humanaigc-outfitanyone.hf.space/--replicas/o90fr/file=/tmp/gradio/65ff53dc99803d44d4e07facd195cda873856968/top5.png",
+    url: "https://humanaigc-outfitanyone.hf.space/--replicas/ppht9/file=/tmp/gradio/65ff53dc99803d44d4e07facd195cda873856968/top5.png",
   },
   {
     id: 2,
-    url: "https://humanaigc-outfitanyone.hf.space/--replicas/o90fr/file=/tmp/gradio/79df244bb1e3a712b3612fdd0bebf93ff40a88a8/top333.png",
+    url: "https://humanaigc-outfitanyone.hf.space/--replicas/ppht9/file=/tmp/gradio/79df244bb1e3a712b3612fdd0bebf93ff40a88a8/top333.png",
   },
   {
     id: 3,
-    url: "https://humanaigc-outfitanyone.hf.space/--replicas/o90fr/file=/tmp/gradio/3b0d73532b2296848b39c332c018211fb1d8f8c6/dress1.png",
+    url: "https://humanaigc-outfitanyone.hf.space/--replicas/ppht9/file=/tmp/gradio/3b0d73532b2296848b39c332c018211fb1d8f8c6/dress1.png",
   },
   {
     id: 4,
-    url: "https://humanaigc-outfitanyone.hf.space/--replicas/o90fr/file=/tmp/gradio/30275a32330224953f3bd08c3e536fd451706bab/dress2.png",
+    url: "https://humanaigc-outfitanyone.hf.space/--replicas/ppht9/file=/tmp/gradio/30275a32330224953f3bd08c3e536fd451706bab/dress2.png",
   },
 ];
 
 const lower_garment = [
   {
     id: 0,
-    url: "https://humanaigc-outfitanyone.hf.space/--replicas/o90fr/file=/tmp/gradio/b231ba8252f242cb5734a28f45b669d4c4bfd965/bottom1.png",
+    url: "https://humanaigc-outfitanyone.hf.space/--replicas/ppht9/file=/tmp/gradio/b231ba8252f242cb5734a28f45b669d4c4bfd965/bottom1.png",
   },
   {
     id: 1,
-    url: "https://humanaigc-outfitanyone.hf.space/--replicas/o90fr/file=/tmp/gradio/17850932298f412c6d307504e6694510c3983592/bottom2.PNG",
+    url: "https://humanaigc-outfitanyone.hf.space/--replicas/ppht9/file=/tmp/gradio/17850932298f412c6d307504e6694510c3983592/bottom2.PNG",
   },
   {
     id: 2,
-    url: "https://humanaigc-outfitanyone.hf.space/--replicas/o90fr/file=/tmp/gradio/166442933d4f5841ac2a19568ac6b1fdfd495e35/bottom3.JPG",
+    url: "https://humanaigc-outfitanyone.hf.space/--replicas/ppht9/file=/tmp/gradio/166442933d4f5841ac2a19568ac6b1fdfd495e35/bottom3.JPG",
   },
   {
     id: 3,
-    url: "https://humanaigc-outfitanyone.hf.space/--replicas/o90fr/file=/tmp/gradio/27316c1d97f31a0eeef64d42202e1beadb08483a/bottom4.PNG",
+    url: "https://humanaigc-outfitanyone.hf.space/--replicas/ppht9/file=/tmp/gradio/27316c1d97f31a0eeef64d42202e1beadb08483a/bottom4.PNG",
   },
   {
     id: 4,
-    url: "https://humanaigc-outfitanyone.hf.space/--replicas/o90fr/file=/tmp/gradio/94c62681b3f3086ea00a63df9a9ceb85fe7b04e8/bottom5.png",
+    url: "https://humanaigc-outfitanyone.hf.space/--replicas/ppht9/file=/tmp/gradio/94c62681b3f3086ea00a63df9a9ceb85fe7b04e8/bottom5.png",
   },
 ];
 
 const models = [
   {
     id: 0,
-    url: "https://humanaigc-outfitanyone.hf.space/--replicas/o90fr/file=/tmp/gradio/91baf552252b7f9c6416f3175f35e98070565a57/Rouyan_0.png",
+    url: "https://humanaigc-outfitanyone.hf.space/--replicas/ppht9/file=/tmp/gradio/91baf552252b7f9c6416f3175f35e98070565a57/Rouyan_0.png",
   },
   {
     id: 1,
-    url: "https://humanaigc-outfitanyone.hf.space/--replicas/o90fr/file=/tmp/gradio/f7dce00cee84dadceb24526d4ce5cc5999855c9d/Rouyan_2.png",
+    url: "https://humanaigc-outfitanyone.hf.space/--replicas/ppht9/file=/tmp/gradio/f7dce00cee84dadceb24526d4ce5cc5999855c9d/Rouyan_2.png",
   },
   {
     id: 2,
-    url: "https://humanaigc-outfitanyone.hf.space/--replicas/o90fr/file=/tmp/gradio/28dbd2deba1e160bfadffbc3675ba4dcac20ca58/Eva_0.png",
+    url: "https://humanaigc-outfitanyone.hf.space/--replicas/ppht9/file=/tmp/gradio/28dbd2deba1e160bfadffbc3675ba4dcac20ca58/Eva_0.png",
   },
   {
     id: 3,
-    url: "https://humanaigc-outfitanyone.hf.space/--replicas/o90fr/file=/tmp/gradio/2f060521cb67195666c59ccd0e47ecc2587a4100/Simon_1.png",
+    url: "https://humanaigc-outfitanyone.hf.space/--replicas/ppht9/file=/tmp/gradio/2f060521cb67195666c59ccd0e47ecc2587a4100/Simon_1.png",
   },
   {
     id: 4,
-    url: "https://humanaigc-outfitanyone.hf.space/--replicas/o90fr/file=/tmp/gradio/3a2287f20c44729f5c7804235f113de32c769baf/Eva_1.png",
+    url: "https://humanaigc-outfitanyone.hf.space/--replicas/ppht9/file=/tmp/gradio/3a2287f20c44729f5c7804235f113de32c769baf/Eva_1.png",
   },
   {
     id: 5,
-    url: "https://humanaigc-outfitanyone.hf.space/--replicas/o90fr/file=/tmp/gradio/9a02f1f9b4db87c22a3341612edeaca4b65920e0/Simon_0.png",
+    url: "https://humanaigc-outfitanyone.hf.space/--replicas/ppht9/file=/tmp/gradio/9a02f1f9b4db87c22a3341612edeaca4b65920e0/Simon_0.png",
   },
   {
     id: 6,
-    url: "https://humanaigc-outfitanyone.hf.space/--replicas/o90fr/file=/tmp/gradio/ef12488138e32ba30930bd342a53fbc409ad00a9/Xuanxuan_0.png",
+    url: "https://humanaigc-outfitanyone.hf.space/--replicas/ppht9/file=/tmp/gradio/ef12488138e32ba30930bd342a53fbc409ad00a9/Xuanxuan_0.png",
   },
   {
     id: 7,
-    url: "https://humanaigc-outfitanyone.hf.space/--replicas/o90fr/file=/tmp/gradio/daca749076bd63a3d8c8a97225a031123e6b432a/Xuanxuan_2.png",
+    url: "https://humanaigc-outfitanyone.hf.space/--replicas/ppht9/file=/tmp/gradio/daca749076bd63a3d8c8a97225a031123e6b432a/Xuanxuan_2.png",
   },
   {
     id: 8,
-    url: "https://humanaigc-outfitanyone.hf.space/--replicas/o90fr/file=/tmp/gradio/951c48e394c297c0632753d1a5fa5b79c59c9a36/Yaqi_1.png",
+    url: "https://humanaigc-outfitanyone.hf.space/--replicas/ppht9/file=/tmp/gradio/951c48e394c297c0632753d1a5fa5b79c59c9a36/Yaqi_1.png",
   },
   {
     id: 9,
-    url: "https://humanaigc-outfitanyone.hf.space/--replicas/o90fr/file=/tmp/gradio/6f793bb1d75f34cdc2c980467ef2f2c242fe9b56/Yifeng_0.png",
+    url: "https://humanaigc-outfitanyone.hf.space/--replicas/ppht9/file=/tmp/gradio/6f793bb1d75f34cdc2c980467ef2f2c242fe9b56/Yifeng_0.png",
   },
   {
     id: 10,
-    url: "https://humanaigc-outfitanyone.hf.space/--replicas/o90fr/file=/tmp/gradio/4c5c245e2e96bf61ea1e606d11e31d0a915575e1/Yifeng_3.png",
+    url: "https://humanaigc-outfitanyone.hf.space/--replicas/ppht9/file=/tmp/gradio/4c5c245e2e96bf61ea1e606d11e31d0a915575e1/Yifeng_3.png",
   },
   {
     id: 11,
-    url: "https://humanaigc-outfitanyone.hf.space/--replicas/o90fr/file=/tmp/gradio/57c66d2b92687a8e1ab12034a9a40d5ae1936ff7/Rouyan_1.png",
+    url: "https://humanaigc-outfitanyone.hf.space/--replicas/ppht9/file=/tmp/gradio/57c66d2b92687a8e1ab12034a9a40d5ae1936ff7/Rouyan_1.png",
   },
   {
     id: 12,
-    url: "https://humanaigc-outfitanyone.hf.space/--replicas/o90fr/file=/tmp/gradio/64f42706321086bbcc24d37a29a5be87da06ef59/Yifeng_2.png",
+    url: "https://humanaigc-outfitanyone.hf.space/--replicas/ppht9/file=/tmp/gradio/64f42706321086bbcc24d37a29a5be87da06ef59/Yifeng_2.png",
   },
   {
     id: 13,
-    url: "https://humanaigc-outfitanyone.hf.space/--replicas/o90fr/file=/tmp/gradio/53974d6ff65483b035a8437be33a9b2c4764b021/Yaqi_0.png",
+    url: "https://humanaigc-outfitanyone.hf.space/--replicas/ppht9/file=/tmp/gradio/53974d6ff65483b035a8437be33a9b2c4764b021/Yaqi_0.png",
   },
 ];
 
@@ -121,7 +121,7 @@ export default function ImagePipeline() {
   useEffect(() => {
     (async () => {
       const a = await client(
-        "https://humanaigc-outfitanyone.hf.space/--replicas/o90fr/",
+        "https://humanaigc-outfitanyone.hf.space/--replicas/ppht9/",
         {}
       );
       setApp(a);
